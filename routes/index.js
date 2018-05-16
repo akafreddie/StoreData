b1e7a8c1885093b777591ea1712a23f4661dcd3f
jenrucho <aanzures@horizon.csueastbayedu>
2018-05-15 20:30:14 -0700

    commit second update

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//now processing post
+router.post('/StoreData', function(req, res, next) {
+//expecting data variable called order--retrieve value using body-parser
+    var value_name = req.body.order  //retrieve the data associated with order
+    res.send("order succesfully received: " + value_name);
+});
 
 module.exports = router;
