a8a2ae7ea3158bf95c1e1bd7c29d205232b75a6f
jenrucho <aanzures@horizon.csueastbayedu>
2018-05-15 20:26:27 -0700

    committing

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 //now processing post
-router.post('/storeData', function(req, res, next) {
+router.post('/StoreData', function(req, res, next) {
 //expecting data variable called order--retrieve value using body-parser
 var value_name = req.body.order  //retrieve the data associated with order
 res.send("order succesfully received: " + value_name);
 });
+
+
 
 module.exports = router;
