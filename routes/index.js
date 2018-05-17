3bef5241f6b08e149626cb120e99ee3b776c1a32
jenrucho <aanzures@horizon.csueastbayedu>
2018-05-17 09:27:39 -0700

    commit fith update( added load to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
+module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://aanzures:mma#1996Aas@ds053708.mlab.com:53708/heroku_25f1f6ms\'';
+
+//LOAD the various controllers
+var controllerMain = require('../controllers/main');   //this will load the main controller file
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,15 +24,17 @@ router.post('/StoreData', function(req, res, next) {
     res.send("order succesfully received: " + value_name);
 });
 
-module.exports = router;
 
 
-var express = require('express');
-var XXX = express.Router();
-var mongodb = require('mongodb');
-var mongoDBURI = process.env.MONGODB_URI || 'mongodb://aanzures:mma#1996Aas@ds053708.mlab.com:53708/heroku_25f1f6ms\'';
 
 
+
+
+//CODE to route /getAllRoutes to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
+//      and Render information iwith an ejs view
+router.get('/getAllOrders', controllerMongoCollection.getAllRoutes);
 
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
