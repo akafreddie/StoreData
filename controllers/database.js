d249cdb738306618f9442646cc217738052a75a7
jenrucho <aanzures@horizon.csueastbayedu>
2018-05-22 08:55:01 -0700

    trying to write to database

--- controllers/database.js
+++ controllers/database.js
@@ -1,6 +1,6 @@
 var mongodb = require('mongodb');
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://aanzures:mma#1996Aas@ds053708.mlab.com:53708/heroku_25f1f6ms';
-
+var router = express.Router();
 
 /** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
  * queries the Routes collection to retrieve all the routes and build the output usig the
@@ -9,7 +9,7 @@ var mongoDBURI = process.env.MONGODB_URI || 'mongodb://aanzures:mma#1996Aas@ds05
  * @param response
  *
  */
-module.exports.getAllOrders =  function (request, response) {
+module.exports.StoreData =  function (request, response) {
 
     mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
         if(err) throw err;
@@ -21,6 +21,33 @@ module.exports.getAllOrders =  function (request, response) {
 
         //get collection of routes
         var Routes = theDatabase.collection('ORDERS');
+        var Routes = theDatabase.collection('SHIPPING');
+        var Routes = theDatabase.collection('BILLING');
+        var Routes = theDatabase.collection('CUSTOMERS');
+
+        //now processing post
+        router.post('/StoreData', function(req, res, next) {
+//expecting data variable called order--retrieve value using body-parser
+            var value_name = req.body.order  //retrieve the data associated with order
+            res.send("order succesfully received: " + value_name);
+        });
+
+        var bodyParser = require('body-parser');
+        var path = require ('path'); //to work with separtors on any OS including Windows
+        var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+        router.use(bodyParser.json()); // for parsing application/json
+        router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+//#########################################
+
+        router.get('/StoreData', function(req, res, next) {
+            //expecting data variable called name --retrieve value using body-parser
+            var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+            var params = JSON.stringify(req.params);//if wanted parameters
+            var query = req.query;  //if wanted the query
+            var value_name = req.query.name;  //retrieve the data associated with name
+            res.send("hello " + value_name);
+        });
 
 
         //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
